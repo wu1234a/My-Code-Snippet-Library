5c11d9ac48548a5bf94e75a3707c5cb5acfaeef0
Shawn <173539799@qq.com>
2018-03-13 13:53:53 +0800

    添加注释

--- JS代码片段.js
+++ JS代码片段.js
@@ -2,11 +2,13 @@
 function siblings(n) {
 	var nodes = [];
 	var prev = n.previousSibling;
+	// 找出元素n前面的所有元素节点
 	while (prev) {
 		if (prev.nodeType === 1) {nodes.push(prev);}
 		prev = prev.previousSibling;
 	}
 	var nexts = n.nextSibling;
+	// 找出元素n后面的所有元素节点
 	while (nexts) {
 		if (nexts.nodeType === 1) {nodes.push(nexts);}
 		nexts = nexts.nextSibling;
