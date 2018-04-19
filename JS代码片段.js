d6275890147f7a1a2a5632aac7f884942649e4b4
Shawn <173539799@qq.com>
2018-04-19 17:17:31 +0800

    增加数组去重功能

--- JS代码片段.js
+++ JS代码片段.js
@@ -27,3 +27,26 @@ function cleanNode(oElement) {
 	}
 	return oElement;
 }
+
+/* 作用：去除数组中重复的元素 
+** 参数：
+** arr     需要去重的数组
+** element 数组元素
+** index   数组索引
+** self    数组本身
+**
+** 方法：
+** filter()
+   创建一个新的数组，新数组中的元素是通过检查指定数组中符合条件的所有元素
+** indexOf() 返回某个指定的字符串值在字符串中首次出现的位置
+**
+** 说明：去除重复元素依靠的是 indexOf 
+** 总是返回第一个元素的位置，后续的重复元素位置与 indexOf 
+** 返回的位置不相等，因此被 filter 滤掉了。
+*/
+function deduplication(arr) {
+	var newarr = arr.filter(function(element, index, self){
+		return self.indexOf(element)===index;
+	});
+	return newarr;
+}
